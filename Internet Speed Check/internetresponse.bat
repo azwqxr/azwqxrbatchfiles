fef0b916736d0bc6f18a97040971520fc5c64d19
azwqxr <130765883+azwqxr@users.noreply.github.com>
2024-10-02 06:19:17 -0600

    Update internetresponse.bat

--- Internet Speed Check/internetresponse.bat
+++ Internet Speed Check/internetresponse.bat
@@ -8,14 +8,17 @@ echo.
 echo Internet Speedtest
 echo.
 echo.
-set /p website="Input website to ping then press enter to begin: "
+powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Enter website (without www.):', 'Internet Response Checker')}" > %TEMP%\out.tmp
+set /p OUT=<%TEMP%\out.tmp
+set msgBoxArgs="& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('You entered: %OUT%', 'Internet Response Checker');}"
+powershell -Command %msgBoxArgs%
 cls
 echo.
 echo.
 echo Internet Speedtest
 echo.
 echo.
-ping www.%website%
+ping www.%out%
 pause
 if /I "%errorlevel%" EQU "1" (
 goto main
