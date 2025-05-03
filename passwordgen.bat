19f40372d142a342132380688cf51e2d2fc8d162
azwqxr <azwqxr@gmail.com>
2025-05-02 19:27:01 -0600

    Add files via upload

---
+++ passwordgen.bat
@@ -0,0 +1,69 @@
+@echo off
+title Password generator
+echo.
+echo.
+echo Random Password generator
+echo.
+echo.
+pause
+cls
+echo.
+echo Loading octo randomization settings...
+timeout /t 2 >nul
+cls
+echo.
+echo Success
+timeout /t 1 >nul
+cls
+echo.
+echo Generating your password now.
+set generatedpword=%random%%random%
+echo.
+echo Current password:%generatedpword%
+timeout /t 1 >nul
+cls
+echo.
+echo Generating your password now..
+set generatedpword=%random%%random%
+echo.
+echo Current password:%generatedpword%
+timeout /t 1 >nul
+cls
+echo.
+echo Generating your password now...
+set generatedpword=%random%%random%
+echo.
+echo Current password:%generatedpword%
+timeout /t 1 >nul
+cls
+echo.
+echo Generating your password now.
+set generatedpword=%random%%random%
+echo.
+echo Current password:%generatedpword%
+timeout /t 1 >nul
+cls
+echo.
+echo Generating your password now..
+set generatedpword=%random%%random%
+echo.
+echo Current password:%generatedpword%
+timeout /t 1 >nul
+cls
+echo.
+echo Generating your password now...
+set generatedpword=%random%%random%
+set generatedpword=%random%%random%
+set generatedpword=%random%%random%
+timeout /t 1 >nul
+cls
+echo.
+echo.
+echo Random Password generator
+echo.
+echo.
+echo Your generated Password: %generatedpword%
+echo.
+echo.
+echo Thank you for using the Random password generator
+pause
